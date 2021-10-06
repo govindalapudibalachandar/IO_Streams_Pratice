03fa554cd5f90932e5c1f9ea9707dd774c4e0270
balachandar <govindalapudibalachandar@gmail.com>
2021-10-06 19:04:14 +0530

    Added:Welcomemessage file is added

---
+++ src/com/bridgelabz/Filter.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Filter {
+    public static void main(String[] args) {
+        System.out.println("Welcome to IOstream");
+    }
+}
