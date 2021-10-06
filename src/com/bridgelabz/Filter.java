484e5f7714030e4d8147a29ca2ca23472ed9b5b9
balachandar <govindalapudibalachandar@gmail.com>
2021-10-06 19:07:22 +0530

    Added:Filter file is added

--- src/com/bridgelabz/Filter.java
+++ src/com/bridgelabz/Filter.java
@@ -7,6 +7,29 @@ import java.util.stream.Collectors;
 
 public class Filter {
     public static void main(String[] args) {
-        System.out.println("Welcome to IOstream");
+        //Using ArrayaList
+   /*     ArrayList<Integer> arraylist = new ArrayList<Integer>();
+        arraylist.add(10);
+        arraylist.add(20);
+        arraylist.add(30);
+        arraylist.add(40);
+        arraylist.add(50);
+        arraylist.add(60);*/
+
+        //used collection arraya
+        List<Integer> arraylist = Arrays.asList(1,2,3,4,5,6);
+        List<Integer> evenarraylist = new ArrayList<>();
+
+        //WithoutStreams
+     /*   for(int n :arraylist)
+        {
+            if (n%2==0)
+                evenarraylist.add(n);
+        }
+        System.out.println(evenarraylist);*/
+
+        //WithStreams
+        evenarraylist = arraylist.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+        System.out.println(evenarraylist);
     }
 }
